7fb99760321a55d612806c4f9b56a2380749e28d
Decadesice <164707712+Decadesice@users.noreply.github.com>
2025-07-14 20:14:55 +0800

    Delete java-ai-langchain4j/Database directory

--- java-ai-langchain4j/Database/console_4.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE appointment (
-    username varchar(50),
-    id_card varchar(50),
-    department varchar(50),
-    `date` varchar(50),
-    `time` varchar(50),
-    doctor_name varchar(50)
-);
-drop table appointment;
